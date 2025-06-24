cd5bae109fb10101b0095bbf7181b99e16af7618
NURLA001 <muratulynurlybek150@gmail.com>
2025-06-24 10:14:15 +0500

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -238,7 +238,7 @@ export default function AdminPage() {
                   : 'text-gray-500 hover:text-gray-700'
               }`}
             >
-              Меню
+              ноове меню
             </button>
             <button
               onClick={() => setActiveTab('categories')}
